db8258f680d1184322df7083628e7bb0e8a07408
Meghana <yeduvakameghana@gmail.com>
2024-09-01 14:02:38 +0530

    Auth

--- littlelemon_finalassessment/restaurant/urls.py
+++ littlelemon_finalassessment/restaurant/urls.py
@@ -1,6 +1,7 @@
 #define URL route for index() view
 from django.contrib import admin 
 from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
 from django.urls import path
 from . import views
 
@@ -8,5 +9,6 @@ urlpatterns = [
  path('', views.index, name='index'),
  path('menu', views.MenuItemsView.as_view()),
  path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+ path('api-token-auth/', obtain_auth_token)
 
 ]
